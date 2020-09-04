2cbfa8278b305a69350bea5d7b74d8217d4fb29e
dumalang <jimmydumalang@gmail.com>
2020-09-05 00:43:16 +0700

    Tricky things to get rid of axios'cache result (Getting the same color all the time)

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 export function loadColor () {
     return (dispatch) => {
-        return axios.get('http://www.colr.org/json/color/random')
+        return axios.get('http://www.colr.org/json/color/random?rand=' + Date.now())
             .then((response) => {
                 dispatch(changeColor('#' + response.data.new_color))
             })
