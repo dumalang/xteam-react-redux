4df883a7ba337a4df87a79fe5b63b0588dd93d4c
jimmy dumalang <jimmy@pawoon.com>
2020-09-04 23:36:17 +0700

    Youtube Step 1 & 2, prepare base components & packages

---
+++ src/component/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import BoxCon from '../container/BoxContainer'
+
+class App extends React.Component {
+    render () {
+        return (
+            <BoxCon></BoxCon>
+        )
+    }
+}
+
+export default App
---
+++ src/container/BoxContainer.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Box from '../component/Box'
+
+class BoxCon extends React.Component {
+    render () {
+        return (
+            <>
+                <Box></Box>
+            </>
+        )
+    }
+}
+
+export default BoxCon
